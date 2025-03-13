79fd5bced47b6da2d3e62cb3f408e18195d58db5
Moldokmatovnurbol <portui0000@gmail.com>
2025-03-13 16:06:49 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import MainPage from "./pages/MainPage";
+
+function App() {
+    return (
+        <div>
+            <MainPage />
+        </div>
+    );
+}
+
+export default App;
